89f7fbd8ca6f9649946bad441994164d90f78635
WrathfulMantis <brandonhope2801@gmail.com>
2024-04-22 01:21:05 +0100

    Added option to apply the default ash of war names to a file

--- CSVProgram.cpp
+++ CSVProgram.cpp
@@ -1332,64 +1332,6 @@ weaponAshFile LoadAshOfWarFile() //add option for filepath
 
     fileToLoad.close();
 
-    //Writing the saved csv to a file
-    std::ofstream fileToWrite;
-    fileToWrite.open("Log.txt");
-
-    //if there is an info line then print that first
-    if (loadFile.infoLine != "")
-    {
-        fileToWrite << loadFile.infoLine << std::endl;
-        fileToWrite.flush();
-        entryAmount -= 1;
-    }
-
-    //Print all lines to file, encoded bytes
-    for (int i = 0; i < entryAmount; i++)
-    {
-        /*
-        fileToWrite << loadFile.ashLineVector.at(i).rowID << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).rowName << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).storedAffinityBytes << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).storedInventoryOptions) << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).storedMainHandWeapons << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).storedOffhandWeapons) << ';';
-
-        fileToWrite << loadFile.ashLineVector.at(i).iconID << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).rank) << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).sortGroupID) << ';';
-
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectID0 << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectID1 << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectID2 << ';';
-
-        fileToWrite << loadFile.ashLineVector.at(i).itemGetTutorialFlagID << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).swortArtParamID << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).mountID << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).sellValue << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).saleValue << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).sortID << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).achievementID << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).achievementSequentialID << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).rarity) << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).showDialogCondType) << ';';
-        fileToWrite << int(loadFile.ashLineVector.at(i).defaultWeaponAffinity) << ';';
-
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectMsgId0 << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectMsgId1 << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectID_ForAttack0 << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectID_ForAttack1 << ';';
-        fileToWrite << loadFile.ashLineVector.at(i).spEffectID_ForAttack2 << ';';
-
-        fileToWrite << loadFile.ashLineVector.at(i).MountWepTextID << ';';
-        */
-
-        fileToWrite << loadFile.DecodedLine(i);
-        fileToWrite << std::endl;
-        fileToWrite.flush();
-    }
-    fileToWrite.close();
-
     return loadFile;
 }
 
@@ -1455,6 +1397,55 @@ void WriteAshOfWarFile(weaponAshFile file, bool encoded) //Add option for filepa
     }
 }
 
+weaponAshFile ApplyDefaultAshNames(weaponAshFile file)
+{
+    class ashName
+    {
+    public:
+        int ashID = 0;
+        std::string ashName;
+    };
+
+    std::vector<ashName> ashNameVector;
+    ashNameVector.reserve(94);
+
+    std::ifstream ashNameFile;
+    ashNameFile.open("DefaultAshNames.txt");
+
+    std::string currentFileLine = "";
+    std::getline(ashNameFile, currentFileLine);
+
+    ashName currentLine;
+    int foundPosision = 0;
+    int stringPosision = 0;
+    for (int i = 0; i < 94; i++)
+    {
+        foundPosision = 0;
+        foundPosision = currentFileLine.find(';');
+        stringPosision = foundPosision + 1;
+        currentLine.ashID = stoi(currentFileLine.substr(0, foundPosision));
+        foundPosision = currentFileLine.find(';', foundPosision + 1);
+        currentLine.ashName = currentFileLine.substr(stringPosision, foundPosision - stringPosision);
+
+        ashNameVector.push_back(currentLine);
+
+        std::getline(ashNameFile, currentFileLine);
+    }
+
+    for (int i = 0; i < file.ashLineVector.size(); i++)
+    {
+        for (int j = 0; j < ashNameVector.size(); j++)
+        {
+            if (file.ashLineVector[i].rowID == ashNameVector[j].ashID)
+            {
+                file.ashLineVector[i].rowName = ashNameVector[j].ashName;
+            }
+        }
+    }
+
+    return file;
+}
+
 void WeaponAshEditor()
 {
     //Start user interface
@@ -1484,8 +1475,13 @@ void WeaponAshEditor()
 int main()
 {
     weaponAshFile ashFile;
+
     ashFile = LoadAshOfWarFile();
+
+    ashFile = ApplyDefaultAshNames(ashFile);
+
     ashFile.EnableAllWeapons();
-    WriteAshOfWarFile(ashFile, 0);
+
+    WriteAshOfWarFile(ashFile, 1);
 }
 
